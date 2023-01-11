ebea5df5b635d244701bb2ebc067ce3043b07157
minemo <wittki56@gmail.com>
2023-01-11 22:33:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+ # Twitch Transcriber
+
+---
+## FAQ:
+### What is this?
+A relatively basic setup for transcribing Livestreams on Twitch using [OpenAI's Whisper](https://github.com/openai/whisper) and [Streamlink](https://github.com/streamlink/streamlink).
+
+### How do use it?
+1. Install ffmpeg with `sudo apt install ffmpeg`, `choco install ffmpeg`...
+2. Install Streamlink with `pip install streamlink`
+3. Create a virtual environment
+4. ```pip install -r requirements.txt```
+5. ````python main.py````
+
+### Why?
+Twitch doesn't have a built-in transcription feature, and I wanted to try out Whisper, so here we are. <br/>
+Feel free to open a Push/Pull Request if you want to add something.
+
+### How does it work?
+I'm sure you can figure it out by reading the code for now. <br/>
+_I'll add a more detailed explanation later, if this turns out to be useful._
+
---
+++ main.py
@@ -0,0 +1,50 @@
+import subprocess
+from io import BytesIO
+import threading
+import whisper
+import time
+
+
+def pipe_stream_to_bytes(username: str, whispermodel: whisper.Whisper, text):
+    """A function that streams a Twitch stream to a BytesIO object."""
+    # Create a subprocess that pipes the stream to ffmpeg
+    process = subprocess.Popen(
+        f"streamlink --loglevel none --twitch-low-latency --twitch-disable-ad twitch.tv/{username} 360p -o - ",
+        stdout=subprocess.PIPE)
+    # Read the stream from the subprocess
+    rectime = 15  # seconds
+    sample_rate = 48000
+    print("Skipping ads, may take a few seconds...")
+    for b in iter(lambda: process.stdout.read(rectime * sample_rate), b''):
+        audio = BytesIO(b)
+        # save to temp file
+        with open("temp.mp4", "wb") as f:
+            f.write(audio.getvalue())
+        # load temp file
+        temp = whispermodel.transcribe("temp.mp4")
+        if temp['text'] != "":
+            text.append(temp['text'])
+
+
+def log_text(alltext, n):
+    # each n seconds, print the text
+    while True:
+        time.sleep(n)
+        if len(alltext) > 0:
+            print(alltext[-1])
+            alltext.clear()
+
+
+if __name__ == '__main__':
+    uname = 'gamesdonequick'
+    print("Loading model...")
+    model = whisper.load_model("base", 'cuda', in_memory=True)
+    print("Done.")
+    # Start the stream in a thread
+    transcribed = []
+    interval = 5
+    print("Starting transcribing and streaming threads...")
+    transcriber = threading.Thread(target=pipe_stream_to_bytes, args=(uname, model, transcribed))
+    logger = threading.Thread(target=log_text, args=(transcribed, interval))
+    transcriber.start()
+    logger.start()
